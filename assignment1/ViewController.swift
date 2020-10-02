1b1a96d006e41cbb1215b36d4954ffea2b6bc2e6
jenniferyuexiao <xiao.yue1@husky.neu.edu>
2020-10-01 23:12:17 -0700

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by 肖玥 on 10/1/20.
+//  Copyright © 2020 yuexiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
